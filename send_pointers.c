f666fd02a30cf239b344190de54315c0e2933a20
WizzOFF <maher_off@proton.me>
2025-03-10 00:01:10 +0100

    send_pointers.c: resolution

--- send_pointers.c
+++ send_pointers.c
@@ -17,7 +17,7 @@ void triplePointeur(int *pointeurSurNombre)
 }
 */
 
-
+/*
 void triplePointeur(int *pointeurSurNombre);
 
 int main(int argc, char *argv[])
@@ -34,4 +34,31 @@ int main(int argc, char *argv[])
 void triplePointeur(int *pointeurSurNombre)
 {
     *pointeurSurNombre *= 3; // On multiplie par 3 la valeur de nombre
+}
+*/
+
+
+void decoupeMinutes(int* pointeurHeures, int* pointeurMinutes);
+
+int main(int argc, char *argv[])
+{
+    int heures = 0, minutes = 90;
+
+    // On envoie l'adresse de heures et minutes
+    decoupeMinutes(&heures, &minutes);
+
+    // Cette fois, les valeurs ont été modifiées !
+    printf("%d heures et %d minutes", heures, minutes);
+
+    return 0;
+}
+
+void decoupeMinutes(int* pointeurHeures, int* pointeurMinutes)
+{
+    /* Attention à ne pas oublier de mettre une étoile devant le nom
+    des pointeurs ! Comme ça, vous pouvez modifier la valeur des variables,
+    et non leur adresse ! Vous ne voudriez pas diviser des adresses,
+    n'est-ce pas ? ;o) */
+    *pointeurHeures = *pointeurMinutes / 60;
+    *pointeurMinutes = *pointeurMinutes % 60; 
 }
