d1100858b1b396240c5402bcdf2c620d4314a16b
WizzOFF <maher_off@proton.me>
2025-03-09 23:55:47 +0100

    send_pointers.c: send a pointer to a function / add a pointer in the main function

--- send_pointers.c
+++ send_pointers.c
@@ -1,3 +1,4 @@
+/*
 void triplePointeur(int *pointeurSurNombre);
 
 int main(int argc, char *argv[])
@@ -6,6 +7,26 @@ int main(int argc, char *argv[])
 
     triplePointeur(&nombre); // On envoie l'adresse de nombre à la fonction
     printf("%d", nombre); // On affiche la variable nombre. La fonction a directement modifié la valeur de la variable car elle connaissait son adresse
+
+    return 0;
+}
+
+void triplePointeur(int *pointeurSurNombre)
+{
+    *pointeurSurNombre *= 3; // On multiplie par 3 la valeur de nombre
+}
+*/
+
+
+void triplePointeur(int *pointeurSurNombre);
+
+int main(int argc, char *argv[])
+{
+    int nombre = 5;
+    int *pointeur = &nombre; // pointeur prend l'adresse de nombre
+
+    triplePointeur(pointeur); // On envoie pointeur (l'adresse de nombre) à la fonction
+    printf("%d", *pointeur); // On affiche la valeur de nombre avec *pointeur
 
     return 0;
 }
